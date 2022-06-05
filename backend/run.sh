a598b9d0dd890b9273515e79e9c33d2b02831517
Aleksandr Sokolov <52139992+bigpe@users.noreply.github.com>
2022-06-06 09:29:45 +1000

    Feature: SSL (https, wss) && Docker deployment

--- backend/run.sh
+++ backend/run.sh
@@ -3,4 +3,4 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-python3 manage.py runserver
+daphne -b 0.0.0.0 rush01.asgi:application
--- backend/rush01/asgi.py
+++ backend/rush01/asgi.py
@@ -9,13 +9,17 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rush01.settings')
+
+import django
+
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 
 from .middleware import AuthMiddlewareFromPath
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rush01.settings')
 
 from ws.notifications import NotificationsConsumer
 
